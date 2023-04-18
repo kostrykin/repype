9acf83a13a82c103892005b8b344fd66cf7b0d9b
Leonid Kostrykin <void@evoid.de>
2023-04-18 18:48:28 +0200

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+with open('pypers/version.py') as fin:
+    exec(fin.read(), globals())
+
+setup(
+    name = 'pypers',
+    version = VERSION,
+    description = 'Powerful batch processing using pipelines for scientific computing.',
+    author = 'Leonid Kostrykin',
+    author_email = 'leonid.kostrykin@bioquant.uni-heidelberg.de',
+    url = 'https://kostrykin.com',
+    license = 'MIT',
+    packages = ['pypers'],
+)
