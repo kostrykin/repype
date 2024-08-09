26ae9540abcc7a98649c3293ab091aebf0b77e7a
Leonid Kostrykin <void@evoid.de>
2024-08-09 20:28:09 +0200

    Remove Python 3.8 from list of tested and supported Python versions

--- setup.py
+++ setup.py
@@ -8,6 +8,7 @@ with open('pypers/version.py') as fin:
 setup(
     name = 'pypers',
     version = VERSION,
+    python_requires = '>3.8',  # requires setuptools>=24.2.0 and pip>=9.0.0
     description = 'Powerful batch processing using pipelines for scientific computing.',
     author = 'Leonid Kostrykin',
     author_email = 'leonid.kostrykin@bioquant.uni-heidelberg.de',
