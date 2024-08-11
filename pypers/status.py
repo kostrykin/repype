kostrykin/repype

commit c17ea3df666022e91c9bf86bea6502f2fe594711

Fix the inscosistent bahaviour of tests in CI

pypers/status.py
@@ -1,5 +1,4 @@
 import json
-import os
 import pathlib
 from .typing import (
     Iterable,
@@ -112,15 +111,6 @@ class StatusReader(FileSystemEventHandler):
         self.update(self.filepath)
 
     def __enter__(self) -> dict:
-        # ================================================================================== #
-        # There is an issue with WatchDog on GitHub Actions, for which this is a workaround. #
-        # Details: https://github.com/kostrykin/pypers/pull/8#issuecomment-2282883353        #
-        if os.environ.get('PYPERS_WATCHDOG_OBSERVER') == 'polling':
-            from watchdog.observers.polling import PollingObserver
-            self.observer = PollingObserver()
-        else:
-            self.observer = Observer()
-        # ================================================================================== #
         self.observer.schedule(self, self.filepath.parent, recursive = False)
         self.observer.start()
         return self.data
@@ -136,6 +126,8 @@ class StatusReader(FileSystemEventHandler):
             return
         
         else:
+            # Load the data from the file, and revert in case of JSON decoding errors
+            # These can occur due to the file being written to while being read, or unfavorable buffer sizes
             try:
                 with open(filepath) as file:
                     data_frame_backup = data_frame.copy()

commit 430d1a24eb4a232a839966d6b51dde8befb01950

Fix bug introduced in c17ea3df666022e91c9bf86bea6502f2fe594711

pypers/status.py
@@ -111,6 +111,7 @@ class StatusReader(FileSystemEventHandler):
         self.update(self.filepath)
 
     def __enter__(self) -> dict:
+        self.observer = Observer()
         self.observer.schedule(self, self.filepath.parent, recursive = False)
         self.observer.start()
         return self.data