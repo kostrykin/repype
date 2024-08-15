d3cf9fed087dfedebedc67ae6d713e8845347e67
Leonid Kostrykin <kostrykin@evoid.de>
2024-08-15 15:19:38 +0200

    Add `long_description` to setup.py

--- setup.py
+++ setup.py
@@ -10,6 +10,8 @@ setup(
     version = VERSION,
     python_requires = '>3.8',  # requires setuptools>=24.2.0 and pip>=9.0.0
     description = 'Reproducible batch processing using pipelines for scientific computing.',
+    long_description = open('README.rst').read(),
+    long_description_content_type = 'text/x-rst',
     author = 'Leonid Kostrykin',
     author_email = 'leonid.kostrykin@bioquant.uni-heidelberg.de',
     url = 'https://github.com/kostrykin/pypers',
