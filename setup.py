8f062294a89e277e79bba379a7d34902fc596104
Leonid Kostrykin <kostrykin@evoid.de>
2024-08-15 14:59:25 +0200

    Change project name to `repype` and add build.yml

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open('pypers/version.py') as fin:
     exec(fin.read(), globals())
 
 setup(
-    name = 'pypers',
+    name = 'repype',
     version = VERSION,
     python_requires = '>3.8',  # requires setuptools>=24.2.0 and pip>=9.0.0
-    description = 'Powerful batch processing using pipelines for scientific computing.',
+    description = 'Reproducible batch processing using pipelines for scientific computing.',
     author = 'Leonid Kostrykin',
     author_email = 'leonid.kostrykin@bioquant.uni-heidelberg.de',
     url = 'https://github.com/kostrykin/pypers',
