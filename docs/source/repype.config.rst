bc55ce04af5f3da2a155dc9af8fe5d0a6f8929b3
Leonid Kostrykin <kostrykin@evoid.de>
2024-08-19 19:52:42 +0200

    Update documentation of `repype.stage`
    xref #13

--- docs/source/repype.config.rst
+++ docs/source/repype.config.rst
@@ -6,4 +6,4 @@ repype.config
     :undoc-members:
     :show-inheritance:
     :special-members:
-    :exclude-members: __dict__, __weakref__, __repr__, __eq__
+    :exclude-members: __dict__, __weakref__, __repr__, __eq__, __annotations__, __init__, __hash__, __module__
--- repype/stage.py
+++ repype/stage.py
@@ -1,12 +1,12 @@
 import hashlib
 import json
-import pathlib
 import re
 import time
 
 import repype.config
 import repype.status
 from repype.typing import (
+    Collection,
     DataDictionary,
     Dict,
     Optional,
@@ -24,7 +24,19 @@ StageEvent = Literal[
     'after',
 ]
 class StageCallback(Protocol):
-    def __call__(self, stage: 'Stage', event: StageEvent, *args, **kwargs) -> None: ...
+    """
+    Stage callback protocol.
+    """
+
+    def __call__(self, stage: 'Stage', event: StageEvent, *args, **kwargs) -> None:
+        """
+        Arguments:
+            stage: The stage that triggered the event.
+            event: The event that triggered the callback.
+            *args: The arguments passed to the callback.
+            **kwargs: The keyword arguments passed to the callback.
+        """
+        pass
 
 
 def suggest_stage_id(class_name: str) -> str:
@@ -35,11 +47,14 @@ def suggest_stage_id(class_name: str) -> str:
     Tokens are grouped if they are consecutive and alphanumeric, but do not start with numbers.
     The function then converts the tokens to lowercase, removes underscores, and joins them with hyphens.
 
-    :param class_name: The name of the class to suggest a configuration namespace for.
-    :type class_name: str
-    :return: A string of hyphen-separated tokens from the class name.
-    :rtype: str
-    :raises AssertionError: If the class name is not valid.
+    Arguments:
+        class_name: The name of the class to suggest a configuration namespace for.
+
+    Returns:
+        A string of hyphen-separated tokens from the class name.
+    
+    Raises:
+        AssertionError: If the class name is not valid.
     """
     assert class_name != '_' and re.match('[a-zA-Z]', class_name) and re.match('^[a-zA-Z_](?:[a-zA-Z0-9_])*$', class_name), f'not a valid class name: "{class_name}"'
     tokens1 = re.findall('[A-Z0-9][^A-Z0-9_]*', class_name)
@@ -64,28 +79,43 @@ class Stage:
     """
     A pipeline stage.
 
-    Each stage can be controlled by a separate set of hyperparameters. Refer to the documentation of the respective pipeline stages for details. Most hyperparameters reside in namespaces, which are uniquely associated with the corresponding pipeline stages.
+    Each stage can be controlled by a separate set of hyperparameters.
+    Those hyperparameters reside in namespaces, which are uniquely associated with the corresponding pipeline stages.
 
-    :param id: The stage ID, used as the hyperparameter namespace. Defaults to the result of the :py:meth:`suggest_stage_id` function if not specified.
-    :param inputs: List of inputs required by this stage.
-    :param consumes: List of inputs consumed by this stage (cannot be used by subsequent stages).
-    :param outputs: List of outputs produced by this stage.
+    Each stage must declare the input fields it requires, and the output fields it produces.
+    These are used by :func:`repype.pipeline.create_pipeline` function to automatically determine the stage order and
+    by the :meth:`repype.pipeline.Pipeline.get_extra_stages` method to determine the stages that are required to be executed additionally.
+    The input field ``input`` is provided by the pipeline itself via the :meth:`repype.pipeline.Pipeline.process` method.
 
-    Automation
-    ^^^^^^^^^^
-
-    Hyperparameters can be set automatically using the :py:meth:`~.configure` method.
-
-    Inputs and outputs
-    ^^^^^^^^^^^^^^^^^^
-
-    Each stage must declare its required inputs and the outputs it produces. These are used by :py:meth:`~.create_pipeline` to automatically determine the stage order. The input ``input`` is provided by the pipeline itself.
+    Arguments:
+        id: The stage ID, used as the hyperparameter namespace.
+            Defaults to the result of the :py:func:`suggest_stage_id` function.
+        inputs: List of inputs required by this stage.
+        consumes: List of inputs consumed by this stage (cannot be used by subsequent stages).
+        outputs: List of outputs produced by this stage.
     """
 
-    inputs   = []
-    outputs  = []
-    consumes = []
-    enabled_by_default = True
+    inputs: Collection[str] = []
+    """
+    List of inputs required by this stage.
+    """
+
+    outputs: Collection[str] = []
+    """
+    List of outputs produced by this stage.
+    """
+
+    consumes: Collection[str] = []
+    """
+    List of inputs consumed by this stage (cannot be used by subsequent stages).
+    """
+
+    enabled_by_default: bool = True
+    """
+    Whether the stage is enabled by default.
+
+    The default value can be overridden by the ``enabled`` hyperparameter of the stage.
+    """
 
     def __init__(self):
         self.id       = type(self).id if hasattr(type(self), 'id') else suggest_stage_id(type(self).__name__)
@@ -97,11 +127,22 @@ class Stage:
         self.event_callbacks: Dict[StageEvent, List[StageCallback]] = dict()
 
     def callback(self, event: StageEvent, *args, **kwargs) -> None:
+        """
+        Call the callbacks for the specified `event`.
+
+        Arguments:
+            event: The event for which to call the callbacks.
+            *args: The arguments to pass to the callbacks.
+            **kwargs: The keyword arguments to pass to the callbacks.
+        """
         if event in self.event_callbacks:
             for callback in self.event_callbacks[event]:
                 callback(self, event, *args, **kwargs)
 
     def add_callback(self, event: StageEvent, callback: StageCallback) -> None:
+        """
+        Add a callback for the specified `event`.
+        """
         if event == 'after':
             self.add_callback( 'end', callback)
             self.add_callback('skip', callback)
@@ -111,6 +152,9 @@ class Stage:
             self.event_callbacks[event].append(callback)
 
     def remove_callback(self, event: StageEvent, callback: StageCallback) -> None:
+        """
+        Remove a callback for the specified `event`.
+        """
         if event == 'after':
             self.remove_callback( 'end', callback)
             self.remove_callback('skip', callback)
@@ -125,6 +169,9 @@ class Stage:
             config: repype.config.Config,
             status: Optional[repype.status.Status] = None,
             **kwargs) -> float:
+        """
+        Run the current stage of the pipeline if it is enabled.
+        """
 
         # Run the stage if it is enabled
         if config.get('enabled', self.enabled_by_default):
@@ -246,14 +293,17 @@ class Stage:
     @property
     def sha(self) -> str:
         """
-        Get a SHA-1 hash which represents the implementation of this stage.
+        Get an SHA-1 hash which represents the implementation of this stage.
 
-        The restrictions of the :py:attr:`~.signature` property apply.
+        The restrictions of the :attr:`signature` property apply.
         """
         signature_str = json.dumps(self.signature)
         return hashlib.sha1(signature_str.encode('utf-8')).hexdigest()
 
     def __str__(self) -> str:
+        """
+        Get a brief string representation of the stage (this is the stage ID).
+        """
         return self.id
 
     def __repr__(self) -> str:
