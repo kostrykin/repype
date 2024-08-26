aa43645f5c10c5c969691048bb7f9344b8ba0c26
Leonid Kostrykin <void@evoid.de>
2024-08-26 16:22:38 +0200

    Add test_textual.py

--- tests/test_textual.py
+++ tests/test_textual.py
@@ -1,5 +1,31 @@
+import unittest
+
 import repype.textual.app
 import repype.textual.batch
 import repype.textual.confirm
 import repype.textual.editor
 import repype.textual.run
+from . import test_repype
+
+
+class Repype(unittest.IsolatedAsyncioTestCase):
+
+    async def asyncSetUp(self):
+        self.repype_segmentation = test_repype.repype_segmentation()
+        self.repype_segmentation.setUp()
+        self.app = repype.textual.app.Repype(path = self.repype_segmentation.root_path)
+
+    async def asyncTearDown(self):
+        self.repype_segmentation.tearDown()
+
+    async def test_screen(self):
+        async with self.app.run_test() as pilot:
+            pass
+            #print(self.app.screen)
+            #await pilot.press('q')
+
+    async def test_quit(self):
+        async with self.app.run_test() as pilot:
+            pass
+            #await pilot.press('q')
+            #self.assertFalse(self.app.is_running)
