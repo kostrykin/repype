3983108d6761abbd545d141bda6b0a835d128209
Leonid Kostrykin <void@evoid.de>
2024-08-27 13:23:30 +0200

    Add tests for `Batch.action_add_task` and `batch.action_edit_task`

--- tests/textual/test_batch.py
+++ tests/textual/test_batch.py
@@ -220,3 +220,95 @@ async def test__reset_task__task1(test_case):
         task2_node = find_tree_node_by_task(task_tree.root, ctx2.task)
         test_case.assertIn('pending', task1_node.label)
         test_case.assertNotIn('pending', task2_node.label)
+
+
+async def test__add_task__none(test_case):
+
+    # Load the batch
+    batch = repype.batch.Batch()
+    batch.load(test_case.root_path)
+
+    # Verify the batch screen and its contents
+    async with test_case.app.run_test() as pilot:
+
+        # Select the root node (not a task)
+        task_tree = test_case.app.screen.query_one('#setup-tasks')
+        task_tree.select_node(task_tree.root)
+
+        # Add sub-task below selected task (none)
+        await pilot.press('a')
+
+        # Verify that nothing happened
+        test_case.assertIsInstance(test_case.app.screen, repype.textual.batch.BatchScreen)
+
+
+async def test__add_task__task1(test_case):
+
+    # Load the batch
+    batch = repype.batch.Batch()
+    batch.load(test_case.root_path)
+
+    # Load the tasks
+    ctx1 = batch.context(test_case.root_path / 'task')
+
+    # Verify the batch screen and its contents
+    async with test_case.app.run_test() as pilot:
+
+        # Select the first task
+        task_tree = test_case.app.screen.query_one('#setup-tasks')
+        task1_node = find_tree_node_by_task(task_tree.root, ctx1.task)
+        task_tree.select_node(task1_node)
+
+        # Add sub-task below selected task
+        await pilot.press('a')
+
+        # Confirm that the editor is shown
+        test_case.assertIsInstance(test_case.app.screen, repype.textual.batch.EditorScreen)
+        test_case.assertEqual(test_case.app.screen.parent_task, ctx1.task)
+        test_case.assertIsNone(test_case.app.screen.my_task)
+
+
+async def test__edit_task__none(test_case):
+
+    # Load the batch
+    batch = repype.batch.Batch()
+    batch.load(test_case.root_path)
+
+    # Verify the batch screen and its contents
+    async with test_case.app.run_test() as pilot:
+
+        # Select the root node (not a task)
+        task_tree = test_case.app.screen.query_one('#setup-tasks')
+        task_tree.select_node(task_tree.root)
+
+        # Edit the selected task (none)
+        await pilot.press('e')
+
+        # Verify that nothing happened
+        test_case.assertIsInstance(test_case.app.screen, repype.textual.batch.BatchScreen)
+
+
+async def test__edit_task__task1(test_case):
+
+    # Load the batch
+    batch = repype.batch.Batch()
+    batch.load(test_case.root_path)
+
+    # Load the tasks
+    ctx1 = batch.context(test_case.root_path / 'task')
+
+    # Verify the batch screen and its contents
+    async with test_case.app.run_test() as pilot:
+
+        # Select the first task
+        task_tree = test_case.app.screen.query_one('#setup-tasks')
+        task1_node = find_tree_node_by_task(task_tree.root, ctx1.task)
+        task_tree.select_node(task1_node)
+
+        # Edit the selected task
+        await pilot.press('e')
+
+        # Confirm that the editor is shown
+        test_case.assertIsInstance(test_case.app.screen, repype.textual.batch.EditorScreen)
+        test_case.assertEqual(test_case.app.screen.my_task, ctx1.task)
+        test_case.assertIsNone(test_case.app.screen.parent_task)
