13458f42b696dd516f80218937e5d6a809805d0d
Leonid Kostrykin <void@evoid.de>
2024-08-27 10:30:00 +0200

    Clean up code

--- tests/test_textual.py
+++ tests/test_textual.py
@@ -23,28 +23,38 @@ class Textual(unittest.IsolatedAsyncioTestCase):
             self.skipTest(f'Textual tests require Python 3.10 or later (found: {platform.python_version()})')
 
         else:
+            # Gather all tests from within the 'textual' directory
             test_filename_pattern = re.compile(r'^test_[a-zA-Z0-9_]+\.py$')
             test_directory_path = pathlib.Path(__file__).parent / 'textual'
-            os.environ['COVERAGE_PROCESS_START'] = '.coveragerc'
-            for filename in os.listdir(test_directory_path):
-                if test_filename_pattern.match(filename):
-                    filepath = test_directory_path / filename
-                    with self.subTest(test = filepath.stem):
+            test_filepaths = [
+                test_directory_path / filename
+                for filename in os.listdir(test_directory_path)
+                if test_filename_pattern.match(filename)
+            ]
 
-                        test_process = await asyncio.create_subprocess_exec(
-                            sys.executable,
-                            '-m'
-                            'tests.test_textual',
-                            filepath.stem,
-                            stdout = asyncio.subprocess.PIPE,
-                            stderr = asyncio.subprocess.PIPE,
-                        )
-                        stdout, stderr = await test_process.communicate()
-                        if stdout:
-                            print(stdout.decode())
-                        if stderr:
-                            exception = dill.loads(stderr)
-                            raise exception
+            # Run each test in a separate process, with coverage measuring enabled
+            # This is necessary, because the method `run_test` of Textual apps can be run only once per process
+            os.environ['COVERAGE_PROCESS_START'] = '.coveragerc'
+            for filepath in test_filepaths:
+                with self.subTest(test = filepath.stem):
+
+                    # Spawn the separate test process
+                    test_process = await asyncio.create_subprocess_exec(
+                        sys.executable,
+                        '-m'
+                        'tests.test_textual',
+                        filepath.stem,
+                        stdout = asyncio.subprocess.PIPE,
+                        stderr = asyncio.subprocess.PIPE,
+                    )
+
+                    # Wait for the test process to finish
+                    stdout, stderr = await test_process.communicate()
+                    if stdout:
+                        print(stdout.decode())
+                    if stderr:
+                        exception = dill.loads(stderr)
+                        raise exception
                     
 
 class TextualTestCase(unittest.TestCase):
@@ -58,8 +68,10 @@ class TextualTestCase(unittest.TestCase):
         self.repype_segmentation.tearDown()
 
 
+# This script is run by the `Textual` test case in a separate process
 if __name__ == '__main__':
 
+    # Parse the test name from the command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('test', type = str)
     args = parser.parse_args()
@@ -67,24 +79,33 @@ if __name__ == '__main__':
     try:
         with contextlib.redirect_stderr(sys.stdout):
             
+            # Enable coverage measurement (if available)
             try:
                 import coverage
                 coverage.process_startup()
             except ImportError:
                 pass
 
+            # Define the co-routine that runs the test
             async def main():
+
+                # Load the test
                 test = importlib.import_module(f'tests.textual.{args.test}')
                 test_case_str = test.test_case
                 test_case_module = importlib.import_module('.'.join(test_case_str.split('.')[:-1]))
+
+                # Instantiate the demanded test case
                 test_case_class  = getattr(test_case_module, test_case_str.split('.')[-1])
                 test_case = test_case_class()
+
+                # Run the test with the demanded test case
                 try:
                     test_case.setUp()
                     await test.run(test_case)
                 finally:
                     test_case.tearDown()
                     
+            # Fire up an event loop and run the test co-routine
             asyncio.run(main()) 
 
     except:
