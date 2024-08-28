5e7303ab64244decc238c39d4621bcb4aa4e2a39
Leonid Kostrykin <void@evoid.de>
2024-08-28 23:42:54 +0200

    Add some `pilot.pause` instructions

--- tests/textual/test_run.py
+++ tests/textual/test_run.py
@@ -245,7 +245,9 @@ async def test__action_cancel(test_case):
             await test_case.app.push_screen(screen)
 
             # Cancel the batch run
+            await pilot.pause(0.1)
             await pilot.press('ctrl+c')
+            await pilot.pause(0.1)
 
             # Confirm
             test_case.assertIsInstance(test_case.app.screen, repype.textual.run.ConfirmScreen)
