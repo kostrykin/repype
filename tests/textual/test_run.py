9c84ab8668dde915a2505b1c7b84fdf198e4b1d7
Leonid Kostrykin <void@evoid.de>
2024-08-29 13:41:20 +0200

    Fix test?

--- tests/textual/test_run.py
+++ tests/textual/test_run.py
@@ -245,8 +245,8 @@ async def test__action_cancel(test_case):
             await test_case.app.push_screen(screen)
 
             # Cancel the batch run
+            await pilot.pause(0.2)
             await pilot.press('ctrl+c')
-            await pilot.pause(0.1)
 
             # Confirm
             test_case.assertIsInstance(test_case.app.screen, repype.textual.run.ConfirmScreen)
